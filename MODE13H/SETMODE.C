74f01a8a762eb5d8814beab3593b080ba1f676b1
John <john.sjostrom@valtech.com>
2023-03-10 08:05:31 +0100

    Adding a draw h line and some pixel testing

--- MODE13H/SETMODE.C
+++ MODE13H/SETMODE.C
@@ -29,17 +29,47 @@ void Plot_Pixel(int x, int y, char color){
 
 void Plot_Pixel_Fast(int x, int y, char color){
   // This assumes that the screen size is fixed at 320x200
-  video_buffer[SCREEN_WIDTH * y + x] = color;
+  video_buffer[((y << 8) + (y << 6)) + x] = color;
+}
+
+void Draw_Horizontal_Line(int x, int y, int length, char color){
+  int i;
+  for(i = x; i < x + length; i++)
+    Plot_Pixel_Fast(i, y, color);
 }
 
 void main(void){
+  //char boo[6] = {'h','e','l','l','o','\0'};
+  //char boo[] = "Foo";
+  unsigned int boo = (1000 << 6) + 768;
+  int i,w,h;
 
   Set_Video_Mode(VGA256);
 
-  for(int i = 0; i < 1000; i++)
-  Fill_Screen(i + 50);
+  // for(i = 0; i < 1000; i++)
+  //   Fill_Screen(i + 50);
+
+  Fill_Screen(230);
+
+  Draw_Horizontal_Line(10, 50, 300, 100);
+
+  for(h = 0; h < SCREEN_HEIGHT; h++){
+    for(w = 0; w < SCREEN_WIDTH; w++){
+        Plot_Pixel(w, h, 100);
+    }
+  }
+
+  Plot_Pixel(150, 150, 220);
+
+//   while(!kbhit()){}
+
+
+
+//   char boo[6] = "hello";
+
+  //printf("%u\n", boo);
+  //printf("%u", sizeof(boo));
 
   while(!kbhit()){}
-
   Set_Video_Mode(TEXT_MODE);
 }
