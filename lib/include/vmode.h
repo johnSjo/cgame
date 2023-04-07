c356fa5820deba62536131051edf13aa152b021e
John <john.sjostrom@valtech.com>
2023-04-07 10:28:51 +0200

    adding wait for v-retrace functionality

--- lib/include/vmode.h
+++ lib/include/vmode.h
@@ -13,13 +13,16 @@
 #define TEXT_MODE 0x03
 #define SCREEN_WIDTH (unsigned int)320
 #define SCREEN_HEIGHT (unsigned int)200
+#define INPUT_STATUS 0x3DA
+#define VRETRACE_BIT 0x08
 
 // GLOBALS
 extern uchar far *video_buffer;
 
 // PROTOTYPES
 void Set_Video_Mode(int mode);
+void wait_for_retrace(void);
 void Plot_Pixel_Fast(int x, int y, char color);
-void Fill_Screen(int color);
+void Fill_Screen(uchar color);
 
 #endif
--- lib/src/vmode.c
+++ lib/src/vmode.c
@@ -14,15 +14,25 @@ void Set_Video_Mode(int mode)
     int86(VIDEO_INTERRUPT, &inregs, &outregs);
 }
 
+void wait_for_retrace()
+{
+    while (inportb(INPUT_STATUS) & VRETRACE_BIT)
+    {
+    }
+    while (!(inportb(INPUT_STATUS) & VRETRACE_BIT))
+    {
+    }
+}
+
 void Plot_Pixel_Fast(int x, int y, char color)
 {
     // This assumes that the screen size is fixed at 320x200
     video_buffer[((y << 8) + (y << 6)) + x] = color;
 }
 
-void Fill_Screen(int color)
+void Fill_Screen(uchar color)
 {
-    _fmemset(video_buffer, (char)color, 64001);
+    _fmemset(video_buffer, color, 64000);
 
     // _fmemset is about twice as fast, probably because it sets 16bit a time, not sure
     // and the asm code can probably be better, but for now I'll stick to memset
